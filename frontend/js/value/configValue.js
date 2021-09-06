d61e3c0f51f83ab525fc56b2d1a5cab69b0999b8
BrunoFVenturini <87661701+BrunoFVenturini@users.noreply.github.com>
2021-09-06 17:18:27 -0300

    Revert "Meu primeiro commit"

--- frontend/js/value/configValue.js
+++
@@ -1,3 +0,0 @@
-angular.module("projetoVenda").value("config", {
-    baseUrl: "http://localhost:8080/api-vendas"
-});
--- projeto/src/main/java/com/projeto/projeto/repo/VendaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.projeto.repo;
-
-import com.projeto.projeto.model.Venda;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VendaRepo extends JpaRepository<Venda, Long> {
-}
